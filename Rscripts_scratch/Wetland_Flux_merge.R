ad8b81dc917a9ae5e3879b25bcd10966289294cf
Keridwen Whitmore <kriddie@Keridwens-MacBook-Pro.local>
2022-10-24 10:38:05 -0500

    oct 8 data script

--- Rscripts_scratch/Wetland_Flux_merge.R
+++ Rscripts_scratch/Wetland_Flux_merge.R
@@ -39,6 +39,11 @@ FluxData$Date<-as.Date(with(FluxData,paste(Year,Month,Day,sep="-")),"%y-%m-%d")
 FluxData$DateTime <- as.POSIXct(paste(FluxData$Date, FluxData$Time), format="%Y-%m-%d %H:%M:%S", tz = "UTC")
 
 FluxData <- FluxData%>%filter(DateTime > "2022-06-01 00:00:00")
+#before october, EOS was in EST, not ecuador time, so subtract 1 hour
+FluxData$DateTime <- FluxData$DateTime - hours(1)
+#FluxData$DateTime_adjusted <- as.POSIXct("2022-08-01 00:00:01", tz = "UTC")
+FluxData[FluxData$DateTime < "2022-08-01 00:00:00",]$DateTime <- FluxData[FluxData$DateTime < "2022-08-01 00:00:00",]$DateTime - hours(1)
+
 
 ggplot(FluxData, aes(x=DateTime, y=Flux)) +
   geom_point(size=2, shape=23)
