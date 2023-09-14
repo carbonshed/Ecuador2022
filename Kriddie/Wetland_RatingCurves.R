420adefbd3d5c834f160ec83938a5328c3e51308
Keridwen Whitmore <kriddie@keridwens-mbp.wireless-1x.unc.edu>
2023-09-14 10:09:58 -0400

    surface area dataframe

--- Kriddie/Wetland_RatingCurves.R
+++ Kriddie/Wetland_RatingCurves.R
@@ -1,13 +1,14 @@
 #wetland depth vs surface area relationships
 library(here)
 
-df <- read.csv(here::here("Wetlands/DroneFiles/Drone_flights_2023-08-25.csv"))[,1:6]
-df$WL <- as.numeric(df$WL)
+df <- read.csv(here::here("Wetlands/SurfaceArea_df.csv"))
+df <- df%>%select(Station,Date,Time_recoreded,Time_used,WaterLevel_m,Area,WLTemp_c)
+df$Date <- as.Date(df$Date)
 
- ggplot(data = df%>%filter(Wetland == "Wetland_3"), aes(x = WL, y = Area, color=Date)) + 
+ ggplot(data = df%>%filter(Station == "WL_Wetland02"), aes(x = WaterLevel_m, y = Area, color=Date)) + 
    geom_point(size=3)
 
-#notes
+ #notes
  #Wetland 1 and 3, revisit
  #I feel certain that, at least one day, wetland 7 was completly dry, but I don't know what day that was
  # wetland 10, revisit
@@ -15,11 +16,18 @@ df$WL <- as.numeric(df$WL)
  
  # also, draw those redlines on rating curves.
  
-p2 <- ggplot(data = df, aes(x = WL, y = Area)) + geom_point()
-p2 + facet_wrap(~Wetland)
-
-
-
+ #wetland 1
+ 
+ 
+ #Wetland 02
+ data <- df%>%filter(Station == "WL_Wetland02")
+ data$WaterLevel_m_1 <- data$WaterLevel_m +1
+ lm_log <- lm(WaterLevel_m_1~log(Area), data)
+ plot(WaterLevel_m_1~yArea, data)
+ curve(coef(lm_log)[1] + 
+         coef(lm_log)[2]*log(WaterLevel_m_1), 
+       add=TRUE, 
+       col = "red")
 
 fig1 <- plot_ly(data=wetland_data ,
         x=~DateTime,y=~WaterLevel_m)
