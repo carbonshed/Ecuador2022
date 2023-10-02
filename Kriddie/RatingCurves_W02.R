b5c9ef9066d07590ba9aadde81f185dc5bbdef18
Kriddie Whitmore <whitmore.kriddie@gmail.com>
2023-10-02 17:33:05 -0400

    working on wetlnd 2 rating curve

--- Kriddie/RatingCurves_W02.R
+++ Kriddie/RatingCurves_W02.R
@@ -1,6 +1,6 @@
 #wetland depth vs surface area relationships
 #Wetland 02
-library(here)
+ library(here)
 library(dplyr)
 library(ggplot2)
 library(plotly)
@@ -25,8 +25,8 @@ df <- df%>%select(Station,Date,Time_recoreded,Time_used,WaterLevel_m,Area,WLTemp
 df$DateTime <- as.POSIXct(paste(df$Date,df$Time_used),format="%m/%d/%Y %H:%M",tz="UTC")
 df <- df%>%filter(Station == Station_name)
 
-df_DSM <- read.csv(here::here("Wetlands/HistogramofDSM_06162022.csv"))
-#df_DSM <- read.csv(here::here("Wetlands/HistogramofDSM_20230128.csv"))
+#df_DSM <- read.csv(here::here("Wetlands/HistogramofDSM_06162022.csv"))
+df_DSM <- read.csv(here::here("Wetlands/HistogramofDSM_20230128.csv"))
 #df_DSM <- read.csv(here::here("Wetlands/DSM_20230128_manual.csv"))
 
 df_merge1 <- df%>%select(c(WaterLevel_m,Area))
@@ -34,11 +34,15 @@ df_merge1$method <- "Manual"
 df_merge2 <- df_DSM%>%select(c(WaterLevel_m,Total_Surface_aream2))
 colnames(df_merge2) <- c("WaterLevel_m","Area")
 df_merge2$method <- "DSM"
+df_merge2$WaterLevel_m <- df_merge2$WaterLevel_m - 3.1 + 0.371258303
 df_merge <- rbind(df_merge1,df_merge2)
 
-ggplot(data = df_merge%>%filter(WaterLevel_m<3)
+df_merge[df_merge$WaterLevel_m < 0.4 & df_merge$method=="DSM",]$WaterLevel_m <- NA
+
+ggplot(data = df_merge%>%filter(WaterLevel_m<1)%>%filter(WaterLevel_m>=-.01)
        , aes(x = WaterLevel_m, y = Area, color=method)) + 
   geom_point(size=3)
+
 
 ggplot(data = WL_df , aes(x=DateTime, y = depth_ave_m)) + geom_line(color="blue", size=1) +
   geom_hline(yintercept=max(df$WaterLevel_m), linetype="dashed", color = "red",size=1)+ 
@@ -62,31 +66,22 @@ count(WL_df%>%
                  WaterLevel_m < min(df$WaterLevel_m,na.rm = TRUE))) / count(WL_df%>%na.omit(WaterLevel_m)) * 100
 
 ##
-ggplot(data = df, aes(x = WaterLevel_m, y = Area, color=Date)) + 
-  geom_point(size=3)
-
-ggplot(data = df_DSM, aes(x = WaterLevel_m, y = Total_Surface_aream2)) + 
-  geom_point(size=3)
-
 
 
 #####Rating curve
- 
+
  
 #Wetland 02
-# based on looking at the long term data, we will say depth = 0 , surface area = 0 at water level = 
-#max: 5836.468147
-
-
-#on DSM created 6/16/2022, 4248.31 altitude = 0.1217370732 m in water level
-WL_06162022 <- df%>%filter(Date=="2022-06-16")%>%select(WaterLevel_m)
-WL_06162022 <- WL_06162022[1,1]
-Area_06162022 <- df%>%filter(Date=="2022-06-16")%>%select(Area)
-Area_06162022 <- Area_06162022[1,1]
+# based on looking at the long term data, we will say depth = 0 , surface area = 0 at water level = -0.061409001
 #Based on imagery from 6/16/2022, the lowest land area not covered by water is 4248.31m
+#max surface area: 5836.468147
 
-#so lets increase by increments of .1to see where it falls on the rating curve
-# 4248.31m + .1m = 4248.41m
+df_merge$depth_ave_m <- df_merge$WaterLevel_m - (-0.061409001)
+
+ggplot(data = df_merge%>%filter(depth_ave_m<1)%>%filter(depth_ave_m>=0)
+       , aes(x = depth_ave_m, y = Area, color=method)) + 
+  geom_point(size=3)
+
 
 
 
@@ -94,10 +89,11 @@ Area_06162022 <- Area_06162022[1,1]
 # y=a+b*ln(x) 
  #fit the model
  
-df_1 <- df%>%filter(Station=="WL_Wetland02")
-weight_values <- c(1,1,1,1,1,1)
-df_1$WaterLevel_m_adjust <- df_1$WaterLevel_m -  df_1[5,"WaterLevel_m"]
-lm_log1p <- lm(Area~log1p(WaterLevel_m_adjust)-1, data=df_1,weight=weight_values)
+df_1 <- df_merge
+#weight_values <- c(1,1,1,1,1,1)
+#df_1$WaterLevel_m_adjust <- df_1$WaterLevel_m -  df_1[5,"WaterLevel_m"]
+lm_log1p <- lm(Area~log1p(depth_ave_m)-1, data=df_1#,weight=weight_values
+               )
 summary(lm_log1p)
 
 #plot
@@ -106,14 +102,14 @@ x_exp <- seq(from = -.3, to = 0.6, by = .01)
 #y_exp = lm_log1p$coefficients[1] +  lm_log1p$coefficients[2] * log1p(x_exp)
 y_exp = lm_log1p$coefficients[1] * log1p(x_exp)
 
-plot_ly(x = df_1$WaterLevel_m_adjust, y=df_1$Area)%>%
+plot_ly(x = df_1$depth_ave_m, y=df_1$Area)%>%
   add_markers(size=4)%>%
   add_lines(x = x_exp, y=y_exp)
 #
 
 df_1$WaterLevel_m_adjust <- df_1$WaterLevel_m -  df_1[5,"WaterLevel_m"]
 
-lm_poly2 <- lm(data = df_1, formula = WaterLevel_m ~ Area + I(Area^2)-1)
+lm_poly2 <- lm(data = df_1, formula = depth_ave_m ~ Area + I(Area^2)-1)
 summary(lm_poly2)
 
 x_poly <- seq(from = -10, to = 5000, by = 1)
